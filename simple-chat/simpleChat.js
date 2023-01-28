91bc065bcebf7b2ebbee106176e455b8297e0ab1
Ritik <rakeshkusrivastav@gmail.com>
2023-01-29 02:09:57 +0530

    complete dummy chat app

--- simple-chat/simpleChat.js
+++ simple-chat/simpleChat.js
@@ -8,9 +8,12 @@ const expressServer=app.listen(9000)
 const io = socketio(expressServer)
 
 io.on('connection', (socket)=>{
-    socket.emit('messageFromServer', {data: 'Welcome to the socket io server'})
-
-    socket.on('messageToServer',(data)=>{
-        console.log(data)
+    socket.emit('messageFromServer',{data:"Welcome to the socketio server"});
+    socket.on('messageToServer',(dataFromClient)=>{
+        console.log(dataFromClient)
+    })
+    socket.on('newMessageToServer',(msg)=>{
+        // console.log(msg)
+        io.emit('messageToClients',{text:msg.text})
     })
 })
